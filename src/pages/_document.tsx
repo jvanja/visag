71f9c5110dfcdcc067d6699f37a44f2f93a3921c
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2024-10-17 10:25:17 +0200

    added domcunet

---
+++ src/pages/_document.tsx
@@ -0,0 +1,37 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document'
+import type { DocumentProps } from 'next/document'
+import i18nextConfig from '../../next-i18next.config'
+
+type Props = DocumentProps & {
+  // add custom document props
+}
+
+class MyDocument extends Document<Props> {
+  render() {
+    const currentLocale =
+      this.props.__NEXT_DATA__.locale ??
+      i18nextConfig.i18n.defaultLocale
+    return (
+      <Html lang={currentLocale}>
+        <Head>
+          <meta charSet="utf-8" />
+          <link
+            href="https://fonts.googleapis.com/css?family=Open+Sans:300,400|Oswald:600&display=optional"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
