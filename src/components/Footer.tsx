d43cd5a44be52fad207df19bd6165a03841bc672
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2024-10-17 15:22:29 +0200

    added about

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,14 @@
-import { useTranslation } from 'next-i18next'
+import { useTranslation } from "next-i18next";
 
 export const Footer = () => {
-
-  const { t } = useTranslation('footer')
+  const { t } = useTranslation("footer");
 
   return (
     <footer>
-      <p>
-        {t('about')} footer
-      </p>
+        {/* <!--Copyright section--> */}
+        <div className="bg-gray-600 p-4 text-center text-sm text-white">
+          &copy; {new Date().getFullYear()} {t('copyright')}  | Visag.rs
+        </div>
     </footer>
-  )
-}
+  );
+};
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,27 +1,148 @@
-import Link from 'next/link'
-// import type {
-//   GetServerSideProps,
-//   InferGetServerSidePropsType,
-// } from 'next'
-
-// import { useTranslation } from 'next-i18next'
-// import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-
-// const AboutPage = ( _props: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-const AboutPage = ( ) => {
+const AboutPage = () => {
   // const { t } = useTranslation()
 
   return (
-    <>
-      <main>
-        <Link href="/">
-          <button type="button">
-            Home
-          </button>
-        </Link>
+      <main className="bg-gray-500 text-white">
+        <div className="px-4 p-8 max-w-4xl m-auto">
+          <h1>Vizuelni Stimulator Glasova (VISAG)</h1>
+          Sluh i vid su u razvoju govora kod deteta veoma povezani.Vizuelne
+          percepcije čine preko 80 procenata svih informacija koje se primaju
+          putem čula. Slušno oštećene osobe su upućene da u komuniciranju više
+          koriste vid. Optičko posmatranje kod čitanja govora sa usta možemo
+          podeliti na : 1. celovito-generalizacija 2. parcijalno-detaljizacija U
+          radu na artikulaciji glasova usredsređeni smo na parcijalno
+          posmatranje kojim obraćamo pažnju na lice onoga koji govori; mimika,
+          izgovor, a posebno na govorne organe. Kada je reč o čulu vida slušno
+          oštecenog deteta, onda mislimo na njegov udeo u čitanju govora sa
+          usta, jer ukoliko je oštećenje sluha veće utoliko je vizuelna
+          percepcija potrebnija. Analogno ovome VISAG je pravi način da pomogne
+          gluvom detetu u artikulaciji onih /nevidljivih/ glasova koji se do
+          sada u velikom procentu nisu mogli iz opravdanih razloga korektno
+          artikulisati. Visag je program za artikulaciju glasova, koji se
+          sastoji od animacije glasova srpskog jezika. Neki konsonanti su dati u
+          logatomima radi lakše artikulacije. Svaki glas je vidljiv prilikom izgovora
+          istog. Na ovaj način deca sa različitim oštećenjima sluha i govora
+          mogu naučiti izgovor svakog glasa. Znači, VISAG im pomaže da sluh
+          donekle kompenzuju vidom kod arikulacije glasova. Naravno, uporedo sa
+          VISAG-om, posebno sa nagluvima je neophodno razvijati i slušnu
+          percepciju koristeći SAFU, individualne slušne amplifikatore, ritmičke
+          stimulacije i sl. Pre nego počnemo koristiti VISAG potrebno je
+          pripremiti kod deteta govorni aparat odgovarajućim vežbama disanja i
+          vežbama motorike govornog aparata.
+          <h2>GOVORNI APARAT</h2>
+          Organi koji učestvuju u obrazovanju zvuka /govora/ nazivaju se
+          govornim aparatom. Govorni aparat sačinjavaju organi koji su uključeni
+          u ovaj proces. U zavisnosti od funkcije delimo ih na: • DISAJNE organe
+          • LARINKS • PRAZNINE iznad larinksa.
+          <h2>I DISAJNE ORGANE sačinjavaju:</h2>
+          1. pluća 2. bronhije 3. traheja /dušnik/
+          <h2>II LARINKS</h2>
+          grkljan se nalazi na raskrsnici disajnog i
+          digestivnog puta u srednjoj liniji prednje strane vrata. Pomoću njega
+          se obrazuje glas.
+          <h2>III PRAZNINE IZNAD LARINKSA</h2> čine tri šupljine: 1.
+          grlena-ždreona 2. usna-između larinksa i usta 3. nosna-šupljina nosa.
+          <h2>VEŽBE MOTORIKE GOVORNOG APARATA</h2>
+          <br />
+          <br />
+          <h2>Vežbe usana</h2>
+          Usne su veoma pokretljive u toku artikulacije velikog broja glasova.
+          One mogu biti:OTVORENE, ZATVORENE, ISPUPČENE, ZAOKRUGLJENE, ELIPSASTE,
+          RAZVUČENE, PODVIJENE, POLUZATVORENE, POLUOTVORENE, ISTURENE I
+          NEUTRALNE. U radu sa ovom decom, posebno u procesu artikulacije veoma
+          su važne vežbe usana. 1. Otvaranje i zatvaranje sa zastajanjem. 2. Sa
+          stisnutim zubima pokretanje usana nagore, nadole, ulevo,udesno. 3.
+          Pokretanje usana u sva četiri pravca sa otvorenim ustima. 4. Isticanje
+          zatvorenih usana i njhovo zaokruživanje 5. Jako otvaranje usta,
+          rastezanje usana i naizmenično razvlačenje usana u stranu. 6.
+          Podizanje gornje usne prema nosu i što niže spuštanje. 7. Naizmenično
+          grickanje gornje pa donje usne i obratno. 8. Imitiranje sisanja,
+          usisavanje usana, oblizivanje-usnom o usnu, davanje poljupca,
+          isturanje i povlačenje usana.
+          <br />
+          9. Razvlačenje uglova usana sa i bez pokazivanja zuba.
+          <h2>Vežbe jezika</h2>
+          Motorika jezika je veoma važna zato što pri građenju glasova jezik
+          učestvuje u artikulaciji velikog broja glasova. Pokretljivost jezika
+          treba vežbati na različite načine. 1. Izbacivanje i uvlačenje jezika.
+          2. Oblizivanje jezikom zuba sa spoljašnje i unutrašnje strane. 3.
+          Oblizivanje usana vrhom jezika / kruženje/ sa unutrašnje i spoljašnje
+          strane. 4. Vrhom jezika /usta su otvorena/ dodirivanje alveola gornjih
+          i donjih sekutića. 5. Vrh jezika povlačiti ka vrhu nosa i ka dnu
+          brade. 6. Povlačenje korena jezika ka mekom nepcu. 7. Brzo pokretanje
+          jezika unutra – napolje - gore – dole i kružno kretanje. 8. Upiranje
+          jezikom u donje i gornje sekutiće. 9. Pravljenje žleba po sredini
+          jezika. 10. Vibriranje vrhom jezika r r r r r r r r r
+          <h2>Vežbe donje vilice</h2>
+          Pravilnost artikulacije zavisi i od položaja i pokretljivosti donje
+          vilice. 1. Otvaranje i zatvaranje vilice. 2. Pokretanje vilice
+          levo-desno, dole –gore sa otvorenim ustima. 3. Pokretanje vilice
+          levo-desno, dole –gore sa zatvorenim ustima. 4. Kruženje donjom
+          vilicom.
+          <h2>VEŽBE DISANJA</h2>
+          Disanje ima izuzetan značaj u životnoj funkciji ćelija i organizma, a
+          takode i osnovni faktor u funkcionisanju govornog aparata. Vazdušna
+          struja je vrlo bitna u govoru. Bez dobrog disanja nema mogućnosti za
+          pravilno formiranje glasa. Pravilno disanje je važno za kvalitetnu
+          artikulaciju. Veoma je značajno sistematski izvoditi vežbe disanja,
+          posebno u predartikulacionom i artikulacionom periodu. Često je
+          kapacitet pluća kod gluve i nagluve dece slabiji nego kod dece koja
+          čuju. Pri izvođenju ovih vežbi treba voditi računa o određenim h i g i
+          j e n s k i m zahtevima: • Vežbe disanja izvoditi u prirodi gde nema
+          prašine i dima ili u čistim provetrenim prostorijama. • Vežbe ne smeju
+          dugo trajati i decu ne zamarati. • Voditi računa o odredenim p e d a g
+          o š k i m zahtevima: • Vežbe treba da budu raznovrsne • Izvode se
+          tačno i precizno • Izvoditi ih na početku časa • Vežbe disanja
+          izvoditi u sedećem,stojećem i ležećem položaju • Vežbe izvoditi bez
+          preterivanja u udisaju i izdisaju • Vežbe disanja izvoditi opušteno •
+          Za vreme vežbi disanja uključiti i čula • Pri izvođenju ovih vežbi
+          koristiti razna didaktička sredstva
+          <h2>Vežbe disanja sa predškolskom decom</h2>
+          1. duvanje u komadiće papira 2. naduvavanje balona 3. pravljenje
+          mehurića od sapunice 4. duvanje u zapaljenu sveću sa različite
+          udaljenosti 5. duvanje igračaka u vodi 6. duvanje kroz cevčicu za sok
+          u cašu sa vodom i pravljenje talasa 7. duvanje u kosu druga ili
+          drugarice 8. duvanje u vrtešku od papira 9. duvanje na frulu ili usnu
+          harmoniku 10. podražavanje disanja životinja 11. duvanje na staklo ili
+          ogledalo da se zamagli 12. naizmenično naduvavanje obraza i udaranje
+          šakom po njima 13. duvanje u šake koje su u obliku trube 14.
+          usisavanje i izduvavanje vazduha kroz zube-vežba za izgovor dentalnih
+          glasova 15. lokomotiva pušta paru f.....š...... 16. zujanje pčela
+          Z......Z......Z...... 17. duvanje u vruće jelo 18. padanje kiše
+          Š......Š....... 19. šum vetra F.........F....... NAPOMENA: Kod
+          izvođenja ovih vežbi treba biti oprezan, da ne bi dovele do neželjenih
+          efekata /provociranje epi napada i sl./
+          <h2>FORMIRANJE GLASOVA</h2>
+          1. Na VISAG-u vizuelno je prikazan položaj artikulacionih organa u
+          izgovaranju konkretnog glasa. 2. Dati punu deskripciju normalne
+          artikulacije i ukazati detetu na šta treba da obrati pažnju u izgovoru
+          toga glasa: položaj usana, otvor usta, veličina usnog ugla, položaj
+          jezika, aktivnost donje vilice, vidljivost vibracije jezika, glasnica,
+          nozdrva itd. 3. Kakva je napetost mišica, odnosno njegov artikulacioni
+          tonus. 4. Razviti glas u tri položaja:inicijalnom, medijalnom i
+          finalnom . 5. Uvežbavanje glasa u cilju njegovog automatizovanog
+          izgovora. U tekstu uz animaciju je dat detaljan opis uloge
+          artikulatora za svaki konkretni glas kao smernice ali i podsetnik
+          defektologu, kako najbolje iskoristiti VISAG.
+          <h2>Funkcija pluća:</h2>
+          Plućni vazduh prolazi kroz usnu duplju slobodno – bez bilo kakvog
+          ometanja -kontinuirano.
+          <h2>Funkcija larinksa</h2>
+          Jedan je od osnovnih činilaca u formiranju glasova. U njemu su
+          smeštene glasnice. Kod izgovora zvučnih glasova, glasnice su stavljene
+          u pokret i proizvode zvučni talas.
+          <h2>Funkcija donje vilice:</h2>
+          Donja vilica može imati položaj disanja kroz nos i njega nazivamo
+          NULTA POZICIJA. U takvom položaju zubni venci gornje i donje vilice
+          nisu u kontaktu i postoji izvestan VILICNI UGAO karakterističan za
+          njenu poziciju pri disanju. Ako je vilični ugao manji od ugla koji
+          donja vilica zauzima pri disanju kroz nos sa zatvorenim ustima, tada
+          bismo njihov položaj označili kao minusan. Veličinu viličnog ugla
+          možemo podeliti na : 1. POLUZATVOREN 2. POLUOTVOREN 3. OTVOREN 4.
+          MINUSNI 5. NULTI.
+        </div>
       </main>
-    </>
-  )
-}
+  );
+};
 
-export default AboutPage
+export default AboutPage;
