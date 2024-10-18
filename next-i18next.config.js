daf2c3938147b934d4cb6c3ea8b161ad6fda5cca
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2024-10-18 10:17:30 +0200

    removed debug

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -6,7 +6,7 @@ module.exports = {
     defaultLocale: "sr",
     locales: ["en", "sr"],
   },
-  debug: true,
+  // debug: true,
   // ns: [ "common", "a", "b", "v", "g", "d", "dj", "e", "zz", "z", "i", "j", "k", "l", "lj", "m", "n", "nj", "o", "p", "r", "s", "t", "cj", "u", "f", "h", "c", "ch", "dz", "sh", ],
   localePath: "public/locales",
 
