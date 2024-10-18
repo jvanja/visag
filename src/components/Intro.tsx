368cfe4e6e5542f12351dea6546e89989ae19f6c
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2024-10-18 10:34:12 +0200

    added gradient

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -8,10 +8,10 @@ import {
 const Intro = () => {
   return (
     <Dialog defaultOpen={true}>
-      <DialogContent className="sm:max-w-[425px]">
+      <DialogContent className="sm:max-w-[650px] bg-gradient text-white p-16 border-0">
         <DialogHeader>
-          <DialogTitle>Visag - Vizuelni stimulator govora</DialogTitle>
-          <DialogDescription>
+          <DialogTitle className="text-white text-3xl tracking-wide">Visag - Vizuelni stimulator govora</DialogTitle>
+          <DialogDescription className="text-slate-50">
             Dobro došli u Visag, vizuelni stimulator govora kojim možete
           </DialogDescription>
         </DialogHeader>
