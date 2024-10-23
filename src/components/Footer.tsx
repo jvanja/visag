dcc6e297d3d489368a9edee358104a8dc811177d
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2024-10-23 10:29:14 +0200

    padding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer>
         {/* <!--Copyright section--> */}
-        <div className="bg-gray-600 p-4 text-center text-sm text-white">
+        <div className="bg-gray-600 p-8 text-center text-sm text-white">
           &copy; {new Date().getFullYear()} {t('copyright')}  | Visag.rs
         </div>
     </footer>
